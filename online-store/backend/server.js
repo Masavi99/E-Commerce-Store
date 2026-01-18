9acb7954c38e11675e884fbc8c6d925f308ff3e1
Masavi Syed <masavi.aiub@gmail.com>
2026-01-18 00:07:27 -0500

    Database Setup

--- online-store/backend/server.js
+++ online-store/backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
-
+import { connectDB } from './lib/db.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,4 +9,5 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
+    connectDB();
 })
